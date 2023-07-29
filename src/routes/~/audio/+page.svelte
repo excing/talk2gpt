53ec0adefbafeba1954584f98f4df2f5a4b95c74
chshqiang <courage.exc@gmail.com>
2023-07-29 23:35:17 +0800

    Update: add audio test

---
+++ src/routes/~/audio/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+	function play() {
+		let player = new Audio('/speech_start.mp3');
+		player.volume = 0.3;
+		player.play();
+
+        setTimeout(() => {
+			let player = new Audio('/speech_stop.mp3');
+			player.volume = 0.3;
+			player.play();
+		}, 1000);
+	}
+</script>
+
+<div>
+	<button on:click={play}>播放</button>
+    <audio controls src="/speech_start.mp3"/>
+</div>
